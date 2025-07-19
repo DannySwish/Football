e58213c08ed34abcf77109fe0c9454dba5fb4ca4
Daniel Pappalardo <danielpappalardo@me.com>
2025-07-19 14:36:24 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Daniel on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
